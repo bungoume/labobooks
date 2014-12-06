440e06ab9c7e5ff56fc3bf915fe8caac81eedac5
umezaki <bungoume3@yahoo.co.jp>
2014-12-06 23:48:23 +0900

    use whitenoise

--- labobooks/labobooks/settings/__init__.py
+++ labobooks/labobooks/settings/__init__.py
@@ -86,3 +86,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
