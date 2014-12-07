f0714fd1af71c6c6fa8e80062a5e582f7541dc22
umezaki <bungoume3@yahoo.co.jp>
2014-12-07 14:57:58 +0900

    add heroku settings

---
+++ labobooks/labobooks/settings/production.py
@@ -0,0 +1,16 @@
+from labobooks.settings import *   # NOQA
+import dj_database_url
+
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+DATABASES = {
+    'default':
+        dj_database_url.config(default=os.environ['DATABASE_URL'])
+}
