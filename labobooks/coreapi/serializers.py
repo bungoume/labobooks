f9b29d9116a87abea2bfd53aa2429f7858dba8b1
umezaki <bungoume3@yahoo.co.jp>
2014-12-14 23:21:46 +0900

    update serializer

--- labobooks/coreapi/serializers.py
+++ labobooks/coreapi/serializers.py
@@ -3,12 +3,15 @@ from core.models import MyBook, BookInfo
 from rest_framework import serializers
 
 
+class BookInfoSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = BookInfo
+
+
 class MyBookSerializer(serializers.HyperlinkedModelSerializer):
+    book_info = BookInfoSerializer('book_info')
+
     class Meta:
         model = MyBook
         # fields = ('url', 'username', 'email', 'groups')
-
-
-class BookInfoSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = BookInfo
