466d2a524dfa0c8c82c7d56663ab2f08f1c0bf47
umezaki <bungoume3@yahoo.co.jp>
2014-12-14 11:11:52 +0900

    add page mock

--- labobooks/webfront/urls.py
+++ labobooks/webfront/urls.py
@@ -7,5 +7,10 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns(
     '',
     url(r'^$', RedirectView.as_view(url='dashboard'), name='go-to-dashboard'),
-    url(r'^dashboard', login_required(TemplateView.as_view(template_name="dashboard.html"))),
+    url(r'^dashboard', TemplateView.as_view(template_name="dashboard.html")),
+    url(r'^indices', TemplateView.as_view(template_name="indices.html")),
+    url(r'^shopping', TemplateView.as_view(template_name="shopping.html")),
+    url(r'^stats', TemplateView.as_view(template_name="stats.html")),
+    url(r'^private', login_required(TemplateView.as_view(template_name="indices.html"))),
+    url(r'^login', TemplateView.as_view(template_name="login.html")),
 )
