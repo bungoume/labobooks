6d639d7321c50bb98e1a81ce2898b305e0d7ad50
umezaki <bungoume3@yahoo.co.jp>
2014-12-26 00:29:32 +0900

    fix push_data bug

--- labobooks/coreapi/serializers.py
+++ labobooks/coreapi/serializers.py
@@ -3,15 +3,14 @@ from core.models import MyBook, BookInfo
 from rest_framework import serializers
 
 
-class BookInfoSerializer(serializers.HyperlinkedModelSerializer):
+class BookInfoSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = BookInfo
 
 
-class MyBookSerializer(serializers.HyperlinkedModelSerializer):
-    book_info = BookInfoSerializer('book_info')
+class MyBookSerializer(serializers.ModelSerializer):
+    bookinfo = BookInfoSerializer(source='book_info', read_only=True)
 
     class Meta:
         model = MyBook
-        # fields = ('url', 'username', 'email', 'groups')
