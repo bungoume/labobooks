46cc920d0be2506d2fb0e6c4bf6462f78c617562
umezaki <bungoume3@yahoo.co.jp>
2015-01-03 00:36:35 +0900

    use name url

--- labobooks/webfront/urls.py
+++ labobooks/webfront/urls.py
@@ -7,10 +7,12 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns(
     '',
     url(r'^$', RedirectView.as_view(url='dashboard'), name='home'),
-    url(r'^dashboard', TemplateView.as_view(template_name="dashboard.html")),
+    url(r'^dashboard', 
+    	login_required(TemplateView.as_view(template_name="dashboard.html")), name='dashboard'),
     url(r'^indices', TemplateView.as_view(template_name="indices.html")),
-    url(r'^shopping', TemplateView.as_view(template_name="shopping.html")),
+    url(r'^shopping', 
+    	login_required(TemplateView.as_view(template_name="shopping.html")), name='shopping'),
     url(r'^stats', TemplateView.as_view(template_name="stats.html")),
     url(r'^private', login_required(TemplateView.as_view(template_name="indices.html"))),
-    url(r'^login$', TemplateView.as_view(template_name="login.html")),
+    url(r'^login$', TemplateView.as_view(template_name="login.html"), name='login'),
 )
