2c5f0bf400244aa427d8f0641bf8214a10d5f234
umezaki <bungoume3@yahoo.co.jp>
2015-03-07 23:53:08 +0900

    remove patterns

--- labobooks/webfront/urls.py
+++ labobooks/webfront/urls.py
@@ -1,11 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 from django.views.generic.base import RedirectView
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^$', RedirectView.as_view(url='dashboard'), name='home'),
     url(r'^dashboard',
         login_required(TemplateView.as_view(template_name="dashboard.html")), name='dashboard'),
@@ -15,4 +14,4 @@ urlpatterns = patterns(
     url(r'^stats', TemplateView.as_view(template_name="stats.html")),
     url(r'^private', login_required(TemplateView.as_view(template_name="indices.html"))),
     url(r'^login$', TemplateView.as_view(template_name="login.html"), name='login'),
-)
+]
