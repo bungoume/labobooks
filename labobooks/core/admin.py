45457ba00dab9911ff6c5b84f7e5ea00a858cbb9
umezaki <bungoume3@yahoo.co.jp>
2015-05-19 23:00:19 +0900

    Add admin view

--- labobooks/core/admin.py
+++ labobooks/core/admin.py
@@ -1,3 +1,7 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from core import models
+
+
+admin.site.register(models.MyBook)
+admin.site.register(models.BookInfo)
